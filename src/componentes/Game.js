8ec5220c5b170bed94688c511b970f15b0b493d9
Jesus <jesusmares82@gmail.com>
2021-02-02 21:50:36 -0600

    v.0.1

---
+++ src/componentes/Game.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import Board from "./Board";
+
+function calculateWinner(board) {
+  const winningPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let index = 0; index < winningPositions.length; index++) {
+    const [a, b, c] = winningPositions[index]; // index = 1 a:3,b:4,c:5
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+
+  return null;
+}
+
+const Game = () => {
+  const [board, setBoard] = useState(new Array(9).fill(null)); // null, "X", "O"
+  const [xIsNext, setXIsNext] = useState(true);
+  const [playWinner, setPlayWinner] = useState("");
+  let winner = calculateWinner(board);
+
+  const Reload = () => {
+    const copiedBoard = [...board];
+    copiedBoard.fill(null);
+    setBoard(copiedBoard.fill(null));
+    setPlayWinner("");
+    setXIsNext(true);
+  };
+
+  const handleSquareChange = (index) => {
+    const copiedBoard = [...board];
+    if (copiedBoard[index] || winner) {
+      setPlayWinner(winner);
+
+      return;
+    }
+
+    copiedBoard[index] = xIsNext ? "X" : "O";
+    setXIsNext(!xIsNext);
+    setBoard(copiedBoard);
+    setPlayWinner(winner);
+  };
+  return (
+    <div>
+      <label>Next Turn: {xIsNext ? "X" : "O"}</label>
+      <Board squares={board} handleClick={handleSquareChange} />
+      <button onClick={Reload}>Nueva Partida</button>
+      <label>WINNER: {winner}</label>
+    </div>
+  );
+};
+
+export default Game;
